e7cd79f0d6f797a497b45f66a45c76542a0b2301
shanshan <shanshan@lifx.co>
2021-12-01 14:09:39 +1100

    Draft

---
+++ src/syncS3.py
@@ -0,0 +1,9 @@
+# import boto3
+
+# s3 =
+
+def output(i):
+    return i
+
+if __name__ == "__main__":
+    print(output(1024))
---
+++ src/test_syncS3.py
@@ -0,0 +1,5 @@
+from .syncS3 import output
+
+def test_output():
+    i = 0
+    assert output(i) == i
